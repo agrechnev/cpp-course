f59dfed71243fbc1da7211706dfba872204c4dba
agrechnev <shrike4625@gmail.com>
2017-09-08 11:32:58 +0300

    e3_3

---
+++ examples/e3_4/main.cpp
@@ -0,0 +1,15 @@
+// Example 3_4 : headers
+// Created by Oleksiy Grechnyev 2017
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "Headers example \n\n";
+    
+    return 0;
+}
+
---
+++ examples/e3_4/myfile.h
@@ -0,0 +1,12 @@
+// Or you can start with
+// #pragrma once
+
+// Classic include guard
+#ifndef _MYFILE_H_
+#define _MYFILE_H_
+
+namespace MyNS{
+    int add(int a, int b);
+]
+
+#endif
