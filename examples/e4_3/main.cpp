2d2a38384a8279419794898e0045d3039ea85397
agrechnev <shrike4625@gmail.com>
2017-09-10 12:03:46 +0300

    Lecture 4

--- examples/e4_3/main.cpp
+++ examples/e4_3/main.cpp
@@ -6,42 +6,50 @@ using namespace std;
 //=========================
 // This example is shamelessly stolen from Wikipedia
 
-class Animal {
-public:
+struct Animal {
+    Animal(double w) : weight(w) {}
     virtual void eat() {
         cout << "Animal::eat()" << endl;
     }
+    double weight;
 };
 
 // Two classes virtually inheriting Animal:
 // If you change "public virtual" -> "public", it would not work
-class Mammal : public virtual Animal {
-public:
+struct Mammal : public virtual Animal {
+    Mammal(double w) : Animal(w*1000) {}
     virtual void breathe() {
         cout << "Mammal::breathe()" << endl;
     }
 };
 
-class WingedAnimal : public virtual Animal {
-public:
+struct WingedAnimal : public virtual Animal {
+    WingedAnimal(double w) : Animal(w*100) {}
     virtual void flap(){
         cout << "WingedAnimal::flap()" << endl;
     }
 };
 
 // A bat is still a winged mammal
-class Bat : public Mammal, public WingedAnimal {
+struct Bat : public Mammal, public WingedAnimal {
+    // Bat calls the Animal(w) constructor also !!!
+    Bat(double w) : Mammal(w), WingedAnimal(w), Animal(w) {}
 };
 //=========================
 int main(){
 
     cout << "Diamond problem and virtual inheritance demo:  \n\n";
 
-    Bat b;
+    Bat b(0.05);
 
     b.eat();
     b.breathe();
     b.flap();
+
+    // Result : 0.05 and not 50 and not 5
+    // Only the Animal(w) in Mammal() is used
+    // Animal(w*1000) and Animal(w*100) are not used when creating Bat!
+    cout << "weight = " << b.weight << endl;
 
     return 0;
 }
