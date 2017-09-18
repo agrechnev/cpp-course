59ea33efeab154b896b75e90d9379ea68417f200
Oleksiy Grechnyev <shrike4625@gmail.com>
2017-09-18 17:27:01 +0300

    started 5_4

---
+++ examples/e5_4/main.cpp
@@ -0,0 +1,20 @@
+// Example 5.4 : type conversions (casts)
+// Created by Oleksiy Grechnyev 2017
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+//==============================
+int main(){
+    using namespace std;
+    {
+        cout << "\nconst_cast  \n\n";
+
+        
+    }
+
+    
+
+    return 0;
+}
