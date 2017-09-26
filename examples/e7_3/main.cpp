2aa12f684dab66d4dd7ce40a37bd6043e20947e1
Oleksiy Grechnyev <shrike4625@gmail.com>
2017-09-26 13:44:40 +0300

    7_3 updated

--- examples/e7_3/main.cpp
+++ examples/e7_3/main.cpp
@@ -19,11 +19,18 @@ void print(const C & c){
     cout << endl;
 }
 //=======================================
-/// Prints a map. 
-template <typename C>
-void printMap(const C & c){
-    for (const auto & e : c)
+/// Prints a map with range for
+template <typename M>
+void printMap(const M & m){
+    for (const auto & e : m)
         cout << e.first << " : " << e.second << endl;
+}
+//=======================================
+/// Prints a map with iterators
+template <typename M>
+void printMap2(const M & m){
+    for (auto it = m.begin(); it != m.end(); ++it)
+        cout << it->first << " : " << it->second << endl;
 }
 
 //==============================
@@ -52,6 +59,43 @@ int main(){
         s.erase(22);
         cout << "s = "; print(s);
         // 1 2 3 5 7 11 13 17 19 23
+    }
+    {
+        cout << "-----------------------------";
+        cout << "\nstd::pair :  \n\n";
+
+        pair<string, int> p1("Maria Traydor", 19);
+        auto p2 = make_pair("Nel Zelpher", 23);   // Note the implicit conversion const char [] -> string
+        pair<string, int> p3;
+        p3 = {"Sophia Esteed", 19};           // const char [] -> string
+
+        cout << p1.first << " : " << p1.second << endl;
+        cout << p2.first << " : " << p2.second << endl;
+        cout << p3.first << " : " << p3.second << endl;
+    }
+
+    {
+        cout << "-----------------------------";
+        cout << "\nstd::map :  \n\n";
+
+        map<string, int> m1{
+                {"Maria Traydor", 19},
+                {"Nel Zelpher", 23}
+        };
+
+        m1.insert({"Mirage Koas", 27});  // Create a new entry
+        m1.insert(make_pair("Sophia Esteed", 19));
+        m1.emplace("Peppita Rossetti", 14);
+        m1["Clair Lasbard"] = 25;  // Create OR change
+
+        m1["Mirage Koas"] -= 1;
+        m1.at("Nel Zelpher") -= 1;
+
+        // Increase all values by 1
+        for (auto & p : m1)
+            p.second += 1;
+
+        printMap(m1);
     }
 
 
